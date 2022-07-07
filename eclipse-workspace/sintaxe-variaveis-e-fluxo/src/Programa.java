aeedb7521d178a95860727bfc3f4cac043ba7596
Mumen Mansour <103611001+MumenMansour@users.noreply.github.com>
2022-07-07 16:33:04 -0300

    Add files via upload

---
+++ eclipse-workspace/sintaxe-variaveis-e-fluxo/src/Programa.java
@@ -0,0 +1,8 @@
+package mumen;
+
+public class Programa {
+	public static void main (String[]args ) {
+		System.out.println("ola mundo");
+	}
+}
+
---
+++ eclipse-workspace/sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,21 @@
+
+public class TestaVariaveis {
+	public static void main (String[]args ) {
+		System.out.println("ola novo teste");		
+		int idade; 
+			idade = 37; 
+			System.out.println(idade);	
+			idade = 30 + 25;
+			System.out.println(idade);	
+			idade = 3*9 + 6;
+			System.out.println(idade);	
+		System.out.println("a idade é " + idade);	
+		double salario ;
+			salario = 1250.70;
+			System.out.println(idade);	
+			System.out.println("a idade é " +idade+" e meu salario é" + salario);	
+		double divisao = 3.14 / 2;
+			System.out.println(divisao);	
+		
+	}
+}
